78026dddc365810701c6785848f8d30b2227c8e9
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-23 06:16:39 +0500

    sanadagi postlar soni

--- get_posts_peer_day.py
+++ get_posts_peer_day.py
@@ -1,7 +1,7 @@
 from read_data import fromJson
+import datetime
 
-
-def get_posts_peer_day(day:str)->int:
+def get_posts_peer_day(data:dict, day:str)->int:
     """
     Return the number of posts for given day
 
@@ -11,4 +11,20 @@ def get_posts_peer_day(day:str)->int:
     Returns: 
         dict: a dictionary with the number of posts for each month
     """
-    return
+    msgs=data.get('messages')
+    ansver={}
+    for mgs in msgs:
+        date=mgs['date']
+        date=datetime.datetime.strptime(date,'%Y-%m-%dT%H:%M:%S')
+        y=date.year
+        d=date.day
+        month=date.month
+        ansver.setdefault(month,{})
+        month_day:dict=ansver[month]
+        month_day['day']=day
+        count=month_day.setdefault('count',0)
+        if day==d:
+            month_day['day']+=1 
+    return ansver
+data=fromJson('data/result.json')
+print(get_posts_peer_day(data,3))
